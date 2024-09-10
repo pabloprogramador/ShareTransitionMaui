f09a67add91709a3c310d6ef77fdcad38a986e15
Pablo <pabloprogramador@gmail.com>
2024-09-10 21:46:09 +0100

    first commit

---
+++ ShareTransitionMaui.Sample/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿namespace ShareTransitionMaui.Sample;
+
+public partial class MainPage : ContentPage
+{
+	bool flag;
+    
+    public MainPage()
+	{
+		InitializeComponent();
+	}
+
+   
+
+    private void OnCounterClicked(object sender, EventArgs e)
+	{
+		
+        double xp = 0;
+        double yp = 0;
+
+        if (!flag)
+		{
+            var pointEnd = pgEnd.GetAbsolutePosition();
+            Console.WriteLine($"::X: {pointEnd.X} , Y: {pointEnd.Y}");
+
+            var point = pgStart.GetAbsolutePosition();
+            Console.WriteLine($"::X: {point.X} , Y: {point.Y}");
+
+            xp = pointEnd.X - point.X;
+            yp = pointEnd.Y - point.Y;
+        }
+
+		flag = !flag;
+		
+
+        pgStart.TranslateTo(xp, yp, 1500, easing:Easing.SpringOut);
+
+		
+    }
+}
+
+
+//[DOTNET] ::X: 419 , Y: 596
+//[DOTNET] ::X: 0 , Y: 176
+//[DOTNET] ::X: 60 , Y: 176
+//[DOTNET] ::X: 60 , Y: 904
+
+//::X: 100.66666666666667 , Y: 307.6666666666667
+//::X: 0 , Y: 97.66666666666669
+//::X: 30 , Y: 97.66666666666669
+//::X: 30 , Y: 467
---
+++ ShareTransitionMaui/ScreenPosition.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.Maui.Handlers;
+using Microsoft.Maui.Platform;
+using Microsoft.Maui.Controls;
+
+#if IOS
+using CoreGraphics;
+using UIKit;
+#endif
+
+#if ANDROID
+using Android.Graphics;
+using Android.Views;
+using Microsoft.Maui.Platform;
+#endif
+
+namespace ShareTransitionMaui
+{
+    public static class ViewExtensions
+    {
+        // Método de extensão para calcular a posição absoluta
+        public static Microsoft.Maui.Graphics.Point GetAbsolutePosition(this IView view)
+        {
+            // Chama a função específica para a plataforma
+#if IOS
+        return GetAbsolutePositioniOS(view);
+#elif ANDROID
+            return GetAbsolutePositionAndroid(view);
+#else
+        return new Point(0, 0); // Outras plataformas podem ser tratadas aqui
+#endif
+        }
+
+#if IOS
+        // Implementação para iOS
+        static Microsoft.Maui.Graphics.Point GetAbsolutePositioniOS(IView view)
+        {
+            // Obter a UIView associada à View MAUI
+            var handler = view?.Handler;
+            var nativeView = handler?.PlatformView as UIView;
+            if (nativeView != null)
+            {
+                CGRect absoluteFrame = nativeView.ConvertRectToView(nativeView.Bounds, null);
+                return new Point(absoluteFrame.X, absoluteFrame.Y);
+            }
+            return new Point(0, 0);
+        }
+
+#elif ANDROID        
+        // Implementação para Android
+        static Microsoft.Maui.Graphics.Point GetAbsolutePositionAndroid(IView view)
+        {
+            // Obter o handler e a View nativa associada
+            var handler = view?.Handler;
+            var nativeView = handler?.PlatformView as Android.Views.View;
+            if (nativeView != null)
+            {
+                int[] location = new int[2];
+                nativeView.GetLocationOnScreen(location);
+                return new Microsoft.Maui.Graphics.Point(location[0]/2, location[1]/2);
+            }
+            return new Microsoft.Maui.Graphics.Point(0, 0);
+        }
+#endif
+
+    }
+}
