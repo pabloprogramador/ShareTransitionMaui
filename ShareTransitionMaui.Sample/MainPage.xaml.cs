f25da0bbcd9305a59eed6a35acd5ae3b0dba4986
Pablo <pabloprogramador@gmail.com>
2024-09-12 12:40:43 +0100

    carousel done

--- ShareTransitionMaui.Sample/MainPage.xaml.cs
+++ ShareTransitionMaui.Sample/MainPage.xaml.cs
@@ -1,38 +1,128 @@
-﻿namespace ShareTransitionMaui.Sample;
+﻿using ShareTransitionMaui.Sample.Models;
+namespace ShareTransitionMaui.Sample;
 
 public partial class MainPage : ContentPage
 {
-	bool flag;
+    private double depth = 300; // FORCE OF PARALLAX
+    private double sizeMin = 120; // FORCE OF PARALLAX
+    private double sizeMax = 200; // FORCE OF PARALLAX
+    private int ajustDevice = DeviceInfo.Current.Platform == DevicePlatform.iOS ? 1 : 1;
+
+    List<Hero> Heroes = new List<Hero>
+	{
+		new Hero{
+			Name="Hulk",
+			Price="€ 34,90",
+			Color1="#47c93a",
+			Color2="#13550d",
+			Image="hulk.png",
+            Size=1
+        },
+        new Hero{
+            Name="Spider",
+            Price="€ 25,00",
+            Color1="#e10000",
+            Color2="#640b0b",
+            Image="spider1.png",
+            Size=.7
+        },
+        new Hero{
+            Name="Spider 2",
+            Price="€ 34,90",
+            Color1="#640b0b",
+            Color2="#350404",
+            Image="spider2.png",
+            Size=.7
+        },
+        new Hero{
+            Name="Ironman",
+            Price="€ 70,20",
+            Color1="#eeaa0e",
+            Color2="#bb6805",
+            Image="iron.png",
+            Size=.7
+        },
+    };
+
     
     public MainPage()
 	{
 		InitializeComponent();
+        this.BindingContext = Heroes;
+        //pgCarousel.ItemsSource = Heroes;
 	}
 
    
 
-    private void OnCounterClicked(object sender, EventArgs e)
-	{
-		
-        double xp = 0;
-        double yp = 0;
+    public void Handle_Scrolled(object sender, ItemsViewScrolledEventArgs e)
+    {
+        var _currentIndex = e.CenterItemIndex;
+        var widthCarousel = pgCarousel.Width - 32;
+        var posOffset = (widthCarousel * (_currentIndex + 1)) - (e.HorizontalOffset / ajustDevice);
+        var pos = (((posOffset * depth) / widthCarousel) - depth);
+        double sizeP = (((posOffset * (sizeMax - sizeMin)) / widthCarousel) + sizeMin) ;
 
-        if (!flag)
-		{
-            var pointEnd = pgEnd.GetAbsolutePosition();
-            Console.WriteLine($"::X: {pointEnd.X} , Y: {pointEnd.Y}");
-
-            var point = pgStart.GetAbsolutePosition();
-            Console.WriteLine($"::X: {point.X} , Y: {point.Y}");
-
-            xp = pointEnd.X - point.X;
-            yp = pointEnd.Y - point.Y;
+        if (sizeP > sizeMax)
+        {
+            sizeP = sizeMax + sizeMax - sizeP;
+            sizeP = Math.Min(sizeP, sizeMax);
+        }
+        else
+        {
+            sizeP = Math.Max(sizeP, sizeMin);
         }
 
-		flag = !flag;
+
+        Heroes[e.LastVisibleItemIndex].Position = pos + depth;
+        Heroes[_currentIndex].Position = pos;
+
+        Heroes[e.LastVisibleItemIndex].Size = sizeMin + (sizeMax - sizeP);
+        Heroes[e.FirstVisibleItemIndex].Size = sizeMin + (sizeMax - sizeP);
+        Heroes[_currentIndex].Size = sizeP;
+
+    }
+
+
+
+
+
+
+    private void OnCounterClicked(object sender, EventArgs e)
+	{
+  //      // Suponha que você tenha um StackLayout chamado stackLayout
+  //      var children = pgVertical.Children;
+
+  //      // Filtrar Labels pelo grupo "group1"
+  //      var group1Labels = children
+  //          .OfType<Label>() // Apenas Labels
+  //          .Where(label => label.ClassId == "group1"); // Filtra pelo ClassId
+
+  //      // Exemplo: alterar a cor de fundo de todas as Labels do grupo "group1"
+  //      foreach (var label in group1Labels)
+  //      {
+  //          label.BackgroundColor = Colors.LightBlue;
+  //      }
+
+
+  //      double xp = 0;
+  //      double yp = 0;
+
+  //      if (!flag)
+		//{
+  //          var pointEnd = pgEnd.GetAbsolutePosition();
+  //          Console.WriteLine($"::X: {pointEnd.X} , Y: {pointEnd.Y}");
+
+  //          var point = pgStart.GetAbsolutePosition();
+  //          Console.WriteLine($"::X: {point.X} , Y: {point.Y}");
+
+  //          xp = pointEnd.X - point.X;
+  //          yp = pointEnd.Y - point.Y;
+  //      }
+
+		//flag = !flag;
 		
 
-        pgStart.TranslateTo(xp, yp, 1500, easing:Easing.SpringOut);
+  //      pgStart.TranslateTo(xp, yp, 1500, easing:Easing.SpringOut);
 
 		
     }
---
+++ ShareTransitionMaui.Sample/Models/Hero.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ShareTransitionMaui.Sample.Models
+{
+	public class Hero : INotifyPropertyChanged
+    {
+		public string Name { get; set; }
+		public string Price { get; set; }
+		public string Color1 { get; set; }
+		public string Color2 { get; set; }
+        public string Image { get; set; }
+
+        private double _position;
+        public double Position
+        {
+            get { return _position; }
+            set
+            {
+                if (_position != value)
+                {
+                    _position = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private double _size;
+        public double Size
+        {
+            get { return _size; }
+            set
+            {
+                if (_size != value)
+                {
+                    _size = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
